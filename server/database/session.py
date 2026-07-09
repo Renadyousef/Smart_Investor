713d159a1a69ab9b8371ebeca386164247aa8825
renad <120721701+Renadyousef@users.noreply.github.com>
2026-07-09 17:24:23 +0300

    session of the DB connection so we use it
    this is how to use the DB look into this file

---
+++ server/database/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import sessionmaker
+from connection import engine
+# This is our connection link to the DB we import it to our controllers then use it as db = SessionLocal()
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
