946a80cf177e6d310d88fc8b28a901637ac6a4ac
renad <120721701+Renadyousef@users.noreply.github.com>
2026-07-10 17:40:30 +0300

    Yfin last 60 days retrived !

--- server/services/stock_prediction_service.py
+++ server/services/stock_prediction_service.py
@@ -2,6 +2,21 @@
         
 #calculate growth % also we fetch current day stock ->generate recommendation
 # services/stock_prediction_service.py
+import yfinance as yf
+
+def get_last_sixty_days_data(stock_name):
+     data = yf.download(
+        stock_name,
+        period="3mo"  # roughly 60 trading days
+    )
+     return data["Close"].tail(60)  #returns the last 60 rows of the DataFrame
+
+# Test for aramco ticker is 2222.SR
+if __name__ == "__main__":
+    prices = get_last_sixty_days_data("2222.SR")
+
+    print(prices)
+    print(type(prices))
 
 def get_stock_prediction(stock_name):
     # fetch last 60 days
